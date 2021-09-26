462c19a9d5c35338cd62e595ed89d777b91305d7
Denis Nikonov <nikon1624@gmail.com>
2021-09-26 21:56:43 +0500

    fix progression

--- src/games/progression.js
+++ src/games/progression.js
@@ -33,7 +33,13 @@ const gameData = {
   getCorrectAnswer(question) {
     const questionArr = question.split(' ');
     const index = questionArr.findIndex((item) => item === '..');
-    const questionNums = questionArr.map((item) => Number(item));
+    const questionNums = questionArr.map((item) => {
+      if (item === '..') {
+        return item;
+      }
+
+      return Number(item);
+    });
     return (questionNums[index - 1] + step).toString();
   },
 };
